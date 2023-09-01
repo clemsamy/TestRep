305036d5c2e9754de59bb49c6c0d7b6eba3a725e
CA0 <CA0@BMSL-R90ZXNTT.home>
2023-09-01 14:18:32 +0100

    First Commit from Eclipse

---
+++ src/main/java/TestPKG/TestPGM.java
@@ -0,0 +1,16 @@
+package TestPKG;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestPGM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Clem\\Automation\\chromedriver-win64\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.google.co.uk");
+
+	}
+
+}
