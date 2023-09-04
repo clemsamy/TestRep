d7c869ec37ec2699d0e1582b5d19fc6d5a6f9b30
CA0 <CA0@BMSL-R90ZXNTT.home>
2023-09-04 16:10:21 +0100

    Commit 4

--- src/main/java/TestPKG/TestPGM.java
+++ src/main/java/TestPKG/TestPGM.java
@@ -1,16 +1,48 @@
 package TestPKG;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 
 public class TestPGM {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+	
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Clem\\Automation\\chromedriver-win64\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.google.co.uk");
+		//driver.get("https://www.freshworks.com/");
+		
 
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://www.freshworks.com/");
+
+	}
+	
+	@Test(priority = 1)
+	public void freshWorkslogoTest() {
+		boolean flag = false;
+			flag = driver.findElement(By.cssSelector("a.logo.logo-fworks")).isDisplayed();
+			Assert.assertTrue(flag);
+	}
+
+	@Test(priority = 2)
+	public void freshWorksTitleTest() {
+		System.out.println("running title test...");
+		System.out.println(driver.getTitle());
+		assertEquals(driver.getTitle(), "A fresh approach to customer engagement");
 	}
 
 }
